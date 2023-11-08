8a90772c8039ec13a3b7d1ea119a72ffc69d169a
s0umalya <soumalya.chk@gmail.com>
2023-11-08 11:19:36 +0530

    Initial commit

---
+++ src/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name topGearMotorsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the topGearMotorsApp
+ */
+angular.module('topGearMotorsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ src/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name topGearMotorsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the topGearMotorsApp
+ */
+angular.module('topGearMotorsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
